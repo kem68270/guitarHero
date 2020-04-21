5e64140816ca4a6fb5d89228d998f075f3522e22
kem68270 <63394677+kem68270@users.noreply.github.com>
2020-04-21 15:12:24 +0200

    main.lua

---
+++ main.lua
@@ -0,0 +1,29 @@
+function love.draw()
+ love.graphics.setColor(0, 0.66, 0.66, 1)
+ love.graphics.rectangle("fill", 300, 300, 50, 30)
+end
+local core = {}
+local key = {}
+local buf = {}
+function key.setting()
+-- Setup keys config
+key[0], key[1], key[2], key[3] ={}, {}, {}, {}
+key[0].x = 100 key[0].y = -50
+key[0].width = 50 key[0].height = 50
+key[0].mode = 0
+key[1].x = 175
+key[1].y = -50
+key[1].wi
+key[1]
+key[1]
+key[2]
+key[2]
+key[2]
+key[2]
+key[2]
+key[3]
+key[3]
+key[3]
+key[3]
+key[3]
+end
